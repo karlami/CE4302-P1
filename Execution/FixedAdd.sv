8588705abeb676670942c5881e66847c3a972632
Cristian Marin <cristian-44@hotmail.es>
2024-03-26 21:58:27 -0600

    Delete Execution/FixedAdd.sv

--- Execution/FixedAdd.sv
+++
@@ -1,8 +0,0 @@
-module FixedAdd #(parameter DATA_WIDTH = 16)(
-    input logic [DATA_WIDTH-1:0] operand1,
-    input logic [DATA_WIDTH-1:0] operand2,
-    output logic [DATA_WIDTH-1:0] result
-);
-
-endmodule
-
